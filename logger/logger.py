8f7b17a947050253c3e88af9bdf23228fe38591d
shujin.huang <shujin.huang@datayes.com>
2019-03-06 10:22:26 +0800

    debug

---
+++ logger/logger.py
@@ -0,0 +1,87 @@
+# coding=utf8
+import logging
+
+logging.basicConfig(filename='example.log', filemode='w', level=logging.DEBUG)
+# example.log 默认与当前文件同级
+# filemode='w' 使得每次运行重写整个文件， 上一次运行的log会被覆盖
+logger = logging.getLogger(__name__)
+
+logger.info("start log second")
+logger.debug("start log debug")  # level=logging.DEBUG这条log才会打印出来
+
+
+#
+# logger = logging.getLogger(__name__)
+# logger.setLevel(logging.DEBUG)
+#
+# # create a file handler
+# handler = logging.FileHandler("test.log")
+# handler.setLevel(logging.DEBUG)
+#
+# # create a logging format
+# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# handler.setFormatter(formatter)
+
+# # add the handlers to the logger
+# logger.addHandler(handler)
+#
+# logger.info('Hello baby')
+# logger.debug('Hello baby debug')
+#
+#
+# logger.info("start log")
+# logger.debug("start log debug")
+
+
+class Logger(object):
+    def __init__(self, name, log_level=logging.DEBUG, file_name="default.log"):
+        name = name.replace('.log', '')
+        logging.basicConfig(level=log_level)
+        logger = logging.getLogger(name)
+        # logger.setLevel(logginlevel)
+
+        self.handler = logging.FileHandler(file_name)
+        self.handler.setLevel(log_level)
+
+        formatter = logging.Formatter('%(asctime)s %(levelname)s:%(name)s %(message)s')
+        self.handler.setFormatter(formatter)
+
+        logger.addHandler(self.handler)
+        self._logger = logger
+
+    def get(self):
+        return self._logger
+
+    def release(self):
+        self.handler.close()
+        self._logger.removeHandler(self.handler)
+
+
+#
+# #
+# # with open(logdir + "/common/logging.yaml") as f:
+# #     ycfg = yaml.load(f)
+# #     ycfg.setdefault('version', 1)
+# #     logging.config.dictConfig(ycfg)
+# # logger = logging.getLogger("data_prd_consensus")
+#
+alogger = Logger('test', logging.DEBUG).get()
+alogger.debug("t aa ")
+#
+# # consensus_logger = logging.getLogger("data_prd_consensus")
+# #
+# # logging.basicConfig(level=logging.DEBUG)
+# # logger = logging.getLogger(__name__)
+# #
+# # logger.info('Start reading database')
+# # # read database here
+# # # records = {'john': 55, 'tom': 66}
+# # # logger.debug('Records: %s', records)
+# # logger.debug('Updating records ...')
+# # # update records here
+# # logger.info('Finish updating records')
+# # logger.debug("debug test")
+#
+# # import logging
+#
+# # https://fangpenlin.com/posts/2012/08/26/good-logging-practice-in-python/
--- pool/apply_async.py
+++ pool/apply_async.py
@@ -41,37 +41,39 @@ if __name__ == '__main__':
             if i.successful():  # 进程函数是否执行成功
                 print(i.get())  # 进程函数返回值
 
-    # apply
-    print('\n--------apply------------')
-    pool = multiprocessing.Pool(processes=4)
-    results = []
-    for i in range(10):
-        msg = 'hello world %d' % i
-        result = pool.apply(func, (msg,))
-        results.append(result)
-    print('apply: 堵塞')  # 执行完func才执行该句
-    pool.close()
-    pool.join()  # join语句要放在close之后
-    print(results)
 
-    # map
-    print('\n--------map------------')
-    args = [1, 2, 4, 5, 7, 8]
-    pool = multiprocessing.Pool(processes=5)
-    return_data = pool.map(func, args)
-    print('堵塞')  # 执行完func才执行该句
-    pool.close()
-    pool.join()  # join语句要放在close之后
-    print(return_data)
-
-    # map_async
-    print('\n--------map_async------------')
-    pool = multiprocessing.Pool(processes=5)
-    result = pool.map_async(func, args)
-    print('ready: ', result.ready())
-    print('不堵塞')
-    result.wait()  # 等待所有进程函数执行完毕
-
-    if result.ready():  # 进程函数是否已经启动了
-        if result.successful():  # 进程函数是否执行成功
-            print(result.get())  # 进程函数返回值
+    #
+    # # apply
+    # print('\n--------apply------------')
+    # pool = multiprocessing.Pool(processes=4)
+    # results = []
+    # for i in range(10):
+    #     msg = 'hello world %d' % i
+    #     result = pool.apply(func, (msg,))
+    #     results.append(result)
+    # print('apply: 堵塞')  # 执行完func才执行该句
+    # pool.close()
+    # pool.join()  # join语句要放在close之后
+    # print(results)
+    #
+    # # map
+    # print('\n--------map------------')
+    # args = [1, 2, 4, 5, 7, 8]
+    # pool = multiprocessing.Pool(processes=5)
+    # return_data = pool.map(func, args)
+    # print('堵塞')  # 执行完func才执行该句
+    # pool.close()
+    # pool.join()  # join语句要放在close之后
+    # print(return_data)
+    #
+    # # map_async
+    # print('\n--------map_async------------')
+    # pool = multiprocessing.Pool(processes=5)
+    # result = pool.map_async(func, args)
+    # print('ready: ', result.ready())
+    # print('不堵塞')
+    # result.wait()  # 等待所有进程函数执行完毕
+    #
+    # if result.ready():  # 进程函数是否已经启动了
+    #     if result.successful():  # 进程函数是否执行成功
+    #         print(result.get())  # 进程函数返回值
