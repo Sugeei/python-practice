3d0b69cb20c5415ab95e770d7d4d56fa9c7d21c7
shujinhuang <215626824@qq.com>
2019-07-30 16:03:37 +0800

    bitree

---
+++ tree/test_tree.py
@@ -0,0 +1,16 @@
+from traverse import Forest
+# import
+import os
+
+dir = os.path.dirname(os.path.realpath(__file__))
+dependenceforest = Forest()
+dependenceforest.build_from(os.path.join(dir, 'factors_dependence.json'))
+dependenceforest.preorder_traverse()
+
+print(dependenceforest.path_to('cashconversioncycle') ==[u'accountspayablestrate', u'accountspayablestdays'])
+print(dependenceforest.path_to('cashconversioncycle'))
+print(dependenceforest.path_to('bias20') ==["ma20"])
+print(dependenceforest.path_to('bias20'))
+print(dependenceforest.path_to('a')==[u'e', u'c', u'd', u'b'])
+print(dependenceforest.path_to('a'))
+# "cashconversioncycle")
