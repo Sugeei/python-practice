308479da9d41506212797a02383c4290ceae4d5e
shujin.huang <shujin.huang@datayes.com>
2019-12-27 19:17:40 +0800

    spider add landchina

--- spider/demo/demo_landchina.py
+++ spider/demo/demo_landchina.py
@@ -3,14 +3,12 @@
 # phantom.exit();
 
 
-
 # from category.listcompanyinfo import infoobj
 # from util..urlhandler import get_soup
-from src.headers import header
+from headers import header
 
 from bs4 import BeautifulSoup
 import requests
-
 
 
 def get_soup(params):
@@ -20,13 +18,13 @@ def get_soup(params):
     soup = BeautifulSoup(source.text, 'lxml')
     return soup
 
+
 url = "https://www.landchina.com/default.aspx?tabid=263&ComName=default"
 params = {
     "url": url,
     "header": header
 }
 t = get_soup(params)
-
 
 # infoobj.get_cn_con(t)
 pass
---
+++ spider/feed/landchina.py
@@ -0,0 +1,4 @@
+
+class FeedLand():
+    def __init__(self):
+        self.url = "https://www.landchina.com/default.aspx?tabid=263&ComName=default"
